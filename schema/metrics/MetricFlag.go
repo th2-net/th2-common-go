8d5bcf09236dabb41b87223cf0e241f6da3144b2
butbaiak <konstantine.butbaia@exactprosystems.com>
2023-02-13 22:58:15 +0400

    Added copyright, exported arbiter variables in PrometheusModule and fixed DisableMonitor method

--- schema/metrics/MetricFlag.go
+++ schema/metrics/MetricFlag.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Exactpro (Exactpro Systems Limited)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package metrics
 
 import (
@@ -6,7 +22,7 @@ import (
 )
 
 type MetricFlag struct {
-	Metric prometheus.Gauge
+	metric prometheus.Gauge
 
 	Enabled bool
 }
@@ -19,7 +35,7 @@ func NewMetricFlag(name string, help string) *MetricFlag {
 		},
 	)
 	return &MetricFlag{
-		Metric:  gauge,
+		metric:  gauge,
 		Enabled: false,
 	}
 }
@@ -44,8 +60,8 @@ func (promMetric *MetricFlag) Disable() {
 
 func (promMetric *MetricFlag) OnValueChange(value bool) {
 	if value {
-		promMetric.Metric.Set(1.0)
+		promMetric.metric.Set(1.0)
 	} else {
-		promMetric.Metric.Set(0.0)
+		promMetric.metric.Set(0.0)
 	}
 }
--- schema/modules/PrometheusModule/PrometheusModule.go
+++ schema/modules/PrometheusModule/PrometheusModule.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Exactpro (Exactpro Systems Limited)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package PrometheusModule
 
 import (
@@ -18,8 +34,8 @@ var prometheusModuleKey = common.ModuleKey(PROMETHEUS_MODULE_KEY)
 type PrometheusModule struct {
 	prometheus *metrics.PrometheusServer
 
-	livenessArbiter  *metrics.FlagArbiter
-	readinessArbiter *metrics.FlagArbiter
+	LivenessArbiter  *metrics.FlagArbiter
+	ReadinessArbiter *metrics.FlagArbiter
 }
 
 func (p *PrometheusModule) GetKey() common.ModuleKey {
@@ -49,8 +65,8 @@ func NewPrometheusModule(provider common.ConfigProvider) common.Module {
 
 	return &PrometheusModule{
 		prometheus:       serv,
-		livenessArbiter:  LivenessArbiter,
-		readinessArbiter: ReadinessArbiter,
+		LivenessArbiter:  LivenessArbiter,
+		ReadinessArbiter: ReadinessArbiter,
 	}
 }
 
